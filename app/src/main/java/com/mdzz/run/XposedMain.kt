985cfc0a8e7be434c41c5ceae2c7dae3305138d1
TinyHai <1035908357@qq.com>
2020-03-05 12:59:19 +0800

    supported HotXposed

---
+++ app/src/main/java/com/mdzz/run/XposedMain.kt
@@ -0,0 +1,39 @@
+package com.mdzz.run
+
+import com.mdzz.run.base.BaseHook
+import com.mdzz.run.util.XSharedPrefUtil
+import de.robv.android.xposed.XC_MethodReplacement
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+import net.androidwing.hotxposed.IHookerDispatcher
+
+class XposedMain : IHookerDispatcher {
+
+    companion object {
+        private const val TAG = "XposedMain"
+    }
+
+    override fun dispatch(lpparam: XC_LoadPackage.LoadPackageParam) {
+        if (lpparam.packageName == BuildConfig.APPLICATION_ID) {
+            hookSelf(lpparam.classLoader)
+            return
+        }
+        if (lpparam.packageName == HOOK_PACKAGE) {
+            XposedBridge.log("Run 已加载")
+            if (XSharedPrefUtil.getBoolean(HOOK_START)) {
+                HKInstrumentation().getClassLoaderAndStartHook(lpparam)
+            }
+        }
+    }
+
+    private fun hookSelf(classLoader: ClassLoader) {
+        try {
+            val mainActivityClass = classLoader.loadClass("com.mdzz.activity.MainActivity")
+            XposedHelpers.findAndHookMethod(mainActivityClass, "isActive",
+                    XC_MethodReplacement.returnConstant(true))
+        } catch (th: Throwable) {
+            BaseHook.log(TAG, th)
+        }
+    }
+}
