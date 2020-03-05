ee9f2f1eaa942f28a3d0331dd71b035b683bd0e2
TinyHai <1035908357@qq.com>
2020-03-05 17:25:16 +0800

    delete useless class

--- app/src/androidTest/java/com/mdzz/hook/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/mdzz/hook/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.mdzz.run
+package com.mdzz.hook
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
--- app/src/androidTest/java/com/mdzz/hook/Test.kt
+++ app/src/androidTest/java/com/mdzz/hook/Test.kt
@@ -1,4 +1,4 @@
-package com.mdzz.run
+package com.mdzz.hook
 
 import android.support.test.runner.AndroidJUnit4
 import org.junit.Test
